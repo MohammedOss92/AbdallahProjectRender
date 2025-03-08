a5cad95439e517f9d02febd127bf8a5d98cf699e
MohammedOss92 <abdallahomar1992@gmail.com>
2025-03-08 22:15:45 +0300

    first commit

--- AbdallahProject/settings.py
+++ AbdallahProject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+(e-cic8l56m(^k9x)$a2vbq4r3^@-p4v5e7p2&1%p2(t-janw'
-
+#SECRET_KEY = 'django-insecure-+(e-cic8l56m(^k9x)$a2vbq4r3^@-p4v5e7p2&1%p2(t-janw'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG=os.environ.get("DEBUG","False").lower()=="true"
+# ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS=os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
@@ -79,6 +81,16 @@ WSGI_APPLICATION = 'AbdallahProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+
+database_url = os.getenv("DATABASE_URL")
+
+if not database_url:
+    raise ValueError("DATABASE_URL environment variable is not set.")
+
+DATABASES = {
+    "default": dj_database_url.parse(database_url)
+}
 
 DATABASES = {
     'default': {
